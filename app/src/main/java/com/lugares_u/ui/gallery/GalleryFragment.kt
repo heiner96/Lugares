a942be614e81ea07e4e3d4265880c2fae733e85a
heiner urena <hurenaz@insservicios.com>
2022-11-28 22:08:35 -0600

    Se guarda el audio y la imagen en Firebase

--- app/src/main/java/com/lugares_u/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/lugares_u/ui/gallery/GalleryFragment.kt
@@ -7,33 +7,74 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.lugares_u.databinding.FragmentGalleryBinding
+import com.lugares_u.model.Lugar
 import com.lugares_u.viewmodel.GalleryViewModel
+import com.lugares_u.viewmodel.LugarViewModel
 
-class GalleryFragment : Fragment() {
+class GalleryFragment : Fragment(), OnMapReadyCallback {
 
     private var _binding: FragmentGalleryBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+
+    //utilizar un objeto para interactuar con la vista /mapa
+    private lateinit var googleMap: GoogleMap
+    private var mapReady = false
+
+    private lateinit var lugarViewModel: LugarViewModel
+
+
+    //esta funcion se ejecuta cuando se crea el activity
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        binding.map.onCreate(savedInstanceState)
+        binding.map.onResume()
+        binding.map.getMapAsync(this)
+
+    }
+
+    //cuando el mapa esta descargado en el terminal
+    override fun onMapReady(map: GoogleMap) {
+        map.let{
+            googleMap = it
+            mapReady = true
+            //Se actualiza el mapa con los lugares registrados
+            lugarViewModel.getLugares.observe(viewLifecycleOwner){ lugares ->
+                updateMap(lugares)
+                //fijaCamara()
+                map.uiSettings.isZoomControlsEnabled = true
+            }
+        }
+    }
+
+    private fun updateMap(lugares: List<Lugar>) {
+        if(mapReady){
+            lugares.forEach{ lugar ->
+                if(lugar.latitud?.isFinite() == true && lugar.longitud?.isFinite() == true){
+                    val marca = LatLng(lugar.latitud, lugar.longitud)
+                    googleMap.addMarker(MarkerOptions().position(marca).title(lugar.nombre))
+                }
+            }
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val galleryViewModel =
-            ViewModelProvider(this).get(GalleryViewModel::class.java)
+         lugarViewModel =
+            ViewModelProvider(this)[LugarViewModel::class.java]
 
         _binding = FragmentGalleryBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
-        val textView: TextView = binding.textGallery
-        galleryViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+
+        return binding.root
     }
 
     override fun onDestroyView() {
